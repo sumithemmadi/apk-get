60975c47f3391d46b2e8f98ea1f740db0d068b5a
sumithemmadi <sumithemmadi244@gmail.com>
2022-09-08 11:26:46 +0530

    updated

--- generate_apk_list.py
+++ generate_apk_list.py
@@ -7,12 +7,16 @@ data = apk_list.text
 lines = data.split("\n")
 tempName = ""
 allApks = ""
+if os.path.exists(os.environ['PWD'] + "/all_apps.txt"):
+    os.remove(os.environ['PWD'] + "/all_apps.txt")
+
 for line in lines:
     app_info = line.split()
     if len(app_info) != 0:
         appName = app_info[1]
-        print(appName)
         if tempName != appName:
+            with open(os.environ['PWD'] + "/all_apps.txt", "a") as output:
+                output.write(app_info[1] + "\n")
             os.makedirs(os.environ['PWD'] + "/apps/" + appName, exist_ok=True)
             if os.path.exists("/apps/{}/app.txt".format(appName)):
                 os.remove("/apps/{}/app.txt".format(appName))
--- main.cpp
+++ main.cpp
@@ -5,12 +5,59 @@
 #include <vector>
 using namespace std;
 
+string * get_apps_with_name()
+{
+  FILE *in;
+  char tmp[256];
+  char str[100];
+  string temp;
+  string * app_array = new string[4];
+  int n = 0;
+  int i = 0;
 
+  in = fopen("all_apps.txt", "r");
+  if (in == NULL)
+  {
+    perror("Error opening input or output file ");
+    exit(EXIT_FAILURE);
+  }
+
+  while (!feof(in))
+  {
+    memset(tmp, 0x00, sizeof(tmp));
+    if (fgets(tmp, 255, in) != NULL)
+    {
+      if (strstr(tmp, "termux") != NULL)
+      {
+        string line = tmp;
+        int i = 0;
+        stringstream ssin(line);
+        while (ssin.good() && i < 4)
+        {
+          ssin >> app_array[i];
+          ++i;
+        }
+      }
+    }
+  }
+  fclose(in);
+  return app_array;
+}
+
+char search_app(char app_name[])
+{
+  cout << "Searching for " << app_name << endl;
+}
 
 int install_app(char apkName[])
 {
   cout << "Installing " << apkName << endl;
-  cout << "Successfully installed " << apkName << endl;
+  string * app_list = get_apps_with_name();
+  for (int i = 0; i < (*(&app_list + 1) - app_list); i++)
+  {
+    cout << app_list[i] << endl;
+  }
+  
   return 0;
 }
 
@@ -69,7 +116,7 @@ int main(int argc, char *argv[])
     }
     else if (strcmp(argv[1], "search") == 0)
     {
-      // search_app(argv[2]);
+      search_app(argv[2]);
     }
     else
     {
